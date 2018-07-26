8d2a1f3ea0aa6e5fcee3256899608c9eac36ce57
queen-cece <ccezeonu3@gmail.com>
2018-07-26 20:02:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
-# dog.rb
+#dog.rb
+class Dog 
+  def initialize (dog)
+    @dog = dog
+  end 
+  dog1 = Dog.new("fido")
+  dog2 = Dog.new("snoopy")
+  dog3 = Dog.new("lassie")
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
 # person.rb
+class Person 
+  def initialize(person)
+    @person = person
+  end
+  person1 = Person.new("Adele_goldberg")
+  person2 = Person.new("Alan_key")
+    
